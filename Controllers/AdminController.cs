355f5962955f12506ab6a5d87854a561b975cff6
avitoria <avitoria@noovi.com.br>
2023-07-12 19:27:04 -0300

    ajustes na conexão com o banco de dados

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTarefaUsuario.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IConfiguration Configuration;
+        public AdminController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.msg = this.Configuration.GetConnectionString("DefaultConnection");
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,8 +2,16 @@ using ApiTarefaUsuario.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Linq;
+using Swashbuckle.Swagger;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,13 +39,52 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddDbContext<DataContext>
     (options => options.UseSqlServer
-    ("Data Source = NOVNTB596; Initial Catalog = ApiTarefaUsuario; Integrated Security = True; TrustServerCertificate = True"));
+    (builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddTransient<ITarefaRepository, TarefaRepository>();
 builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Tarefa Usuario API",
+        Version = "v1"
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer 12345abcdef\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                new string[] {}
+            }
+        }
+    );
+});
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tarefa-Usuario API V1");
+});
 
 app.UseRouting();
 
